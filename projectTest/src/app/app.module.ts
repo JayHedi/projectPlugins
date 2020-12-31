cdc48ea8dc5a5f5c0052ee38d4b14f5ad22e14ca
Hedi Jebali <h.jebali@itdmgroup.it>
2020-12-31 15:45:50 +0100

    created component two under f2

--- projectTest/src/app/app.module.ts
+++ projectTest/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ComponentTwoComponent } from './component-two/component-two.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponentTwoComponent
   ],
   imports: [
     BrowserModule,
